3eb83b43d0b950ffbc78e0d12e0b5b1fa0c098e7
anas-ai <anasrizvi4206@gmail.com>
2026-01-15 00:25:05 +0530

    remove .env from git tracking

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
 
+const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is missing in environment variables");
+  }
 
-dotenv.config();
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+};
 
-const connectDB = async (): Promise<void> => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI as string);
-        console.log('MongoDB connected');
-
-    } catch (error) {
-        console.log("mongoDB connection error:", error);
-        process.exit(1)
-    }
-}
-
-export default connectDB
+export default connectDB;
